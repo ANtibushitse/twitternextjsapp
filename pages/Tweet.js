db11892327499bf1f4cbc63cda4bddedd518db57
Antibushitse <ntibushitse.arsene@gmail.com>
2021-04-05 12:42:45 +0200

    tweet comonent

---
+++ pages/Tweet.js
@@ -0,0 +1,8 @@
+import react from 'react'
+
+const Tweet =({text}) =>{ 
+    const text ='this iis an exemple'
+  return <h2> {text}</h2>
+}
+ 
+export default Tweet
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Tweet from './Tweet'
 
 export default function Home() {
   return (
