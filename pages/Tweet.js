02663b851077300fc2766910bda44214bd74ae03
Antibushitse <ntibushitse.arsene@gmail.com>
2021-04-05 17:26:43 +0200

    basic stylizing with react-bootstrap

--- pages/Tweet.js
+++ pages/Tweet.js
@@ -1,8 +1,21 @@
 import react from 'react'
+ import  {Card} from 'react-bootstrap'
 
-const Tweet =({text}) =>{ 
+const Tweet =({text,author, date}) =>{ 
+
   
-  return <h3 >{text.toUpperCase()}</h3>
+  return <div>
+  <Card style={{ width: '42rem', margin:"auto", marginBottom:"30px"}}>
+  <Card.Body>
+    <Card.Title>{author}</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">{date}</Card.Subtitle>
+    <Card.Text>
+      {text}
+    </Card.Text>
+   
+  </Card.Body>
+</Card>
+  </div>
 }
  
 export default Tweet
