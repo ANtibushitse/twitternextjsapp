092ffdafd69184caf54eae4decbc19c47e10b9a8
Antibushitse <ntibushitse.arsene@gmail.com>
2021-04-05 13:49:48 +0200

    Fetching  tweets

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,25 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Tweet from './Tweet'
 
-export default function Home() {
+import path from 'path'
+import fs from 'fs'
+
+export default function Home({tweets}) {
   return (
     <div className={styles.container}>
     <h1> Hello World! </h1>
-    <Tweet  text= 'pass that prop' author="Arsene"  occupation ="Musician/Producert" />
+    {tweets.map(tweet=> <Tweet key={tweet.id} text= {tweet.text} author={tweet.author} occupation={tweet.occupation} />)}
+    
     </div>
-  )
+  ) 
 }
+
+ export async function getServerSideProps(){
+   const  filePath = path.join(process.cwd(), 'data.json')
+   const tweets  = JSON.parse(fs.readFileSync(filePath))
+   return {
+    props: {
+tweets
+    }
+   }
+ }
